4359faead8acd68d0a31a7fbfb61699bd30d64f5
MaoMantish <mauricio@8manos.com>
2013-11-18 11:55:46 -0500

    fix html de obra destacada en home

--- wp-content/themes/museobog/loop-featured.php
+++ wp-content/themes/museobog/loop-featured.php
@@ -1,15 +1,15 @@
 <?php
-$carousel_query = new WP_Query(array(
+$featured_query = new WP_Query(array(
 	'post_type' => 'work',
 	'posts_per_page' => 1
 ));
 
-while ( $carousel_query->have_posts() ) : $carousel_query->the_post();
+while ( $featured_query->have_posts() ) : $featured_query->the_post();
 ?>
 
 	<?php the_post_thumbnail('wide'); ?>
 	<div class="social-share"></div>
-	<p><?php the_excerpt(); ?></p>
+	<?php the_excerpt(); ?>
 	<p><a class="button" href="<?php the_permalink(); ?>"><?php _e('más información', 'museobog'); ?></a></p>
 
 <?php
