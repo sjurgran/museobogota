03b3a2c98aa6714a8393829944d9fcfd79263744
juan2ramos <juan2ramos@gmail.com>
2013-11-20 11:09:48 -0500

    redes

--- wp-content/themes/museobog/loop-featured.php
+++ wp-content/themes/museobog/loop-featured.php
@@ -8,9 +8,23 @@ while ( $featured_query->have_posts() ) : $featured_query->the_post();
 ?>
 
 	<?php the_post_thumbnail('wide'); ?>
-	<div class="social-share"></div>
+    <div class="social-share">
+        <ul>
+            <li>
+                <a title="facebook" href="http://facebook.com"></a>
+                <span>Like</span>
+                <div class="box-likes">106</div>
+            </li>
+            <li>
+                <a title="twitter" href="http://twitter.com"></a>
+                <span>Tweet</span>
+                <div class="box-likes">61</div>
+            </li>
+            <li><a title="flickr" href="http://flickr.com"></a></li>
+        </ul>
+    </div>
 	<?php the_excerpt(); ?>
-	<p><a class="button" href="<?php the_permalink(); ?>"><?php _e('más información', 'museobog'); ?></a></p>
+    <p class="center" ><a class="button" href="<?php the_permalink(); ?>"><?php _e('más información', 'museobog'); ?></a></p>
 
     <?php
 endwhile;
