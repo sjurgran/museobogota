a7a5e9e27deb21f1c275731a957422ec0d593836
MaoMantish <mauricio@8manos.com>
2013-11-27 12:22:35 -0500

    template error 404 traducciones varias

---
+++ wp-content/themes/museobog/404.php
@@ -0,0 +1,7 @@
+<main role="main" id="main">
+
+	<article>
+		<h1><?php _e('No hemos encontrado lo que buscabas', 'museobog'); ?></h1>
+
+		<p><?php printf( __( 'Puedes regresar al <a href="%s">inicio</a> o realizar una busqueda.', 'museobog' ), esc_url( home_url('/') ) ); ?></p>
+	</article>
